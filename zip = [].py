6c1d53fde314bb9835c1151c15d1a77c9d28ae47
Awesome Person Games <109925719+mkr041107@users.noreply.github.com>
2024-10-04 09:12:18 -0500

    Add files via upload

--- zip = [].py
+++ zip = [].py
@@ -5,13 +5,18 @@ name = []
 address = []
 city = []
 csvfile = 'C:\\Users\\334706\\Downloads\\addresses.csv'
-with open(csvfile, 'r') as f:
+with open(csvfile, 'r', newline = '') as f:
         state = []
-        # create a list of rows in the CSV file
+        has_header = csv.Sniffer().has_header(f.read(1024))
         rows = f.readlines()
-
-        # strip white-space and newlines
         rows = list(map(lambda x:x.strip(), rows))
+        
+        # create a list of rows in the CSV file
+       
+        if has_header:
+            next(f)  # Skip header row.
+        # strip white-space and newlines
+
         for row in rows:
             
             # further split each row into columns assuming delimiter is comma 
